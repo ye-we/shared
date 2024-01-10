76abb7ca7299302662b56a048ba9d6eafd174ebb
Yewe <khalid.yewe@gmail.com>
2024-01-10 11:24:07 +0300

    auth scaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/singin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
 
 const app = express();
 app.use(express.json());
+
+// routers
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 
 app.listen(4000, () => {
   console.log("Running at 4000");
