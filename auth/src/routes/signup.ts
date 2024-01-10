e906be9ff66ea9aff741f6451d464cdef8d5b7f5
Yewe <khalid.yewe@gmail.com>
2024-01-10 11:42:16 +0300

    error validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("hello!!!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 12 })
+      .withMessage("Password must be between 6 and 12 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    console.log("User created");
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
